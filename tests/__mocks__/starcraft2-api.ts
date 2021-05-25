ca0b7b57f484f4bfd22fde1ac36a8d694bde542f
Łukasz Wójcik <lwojcik@lukem.net>
2021-05-25 21:12:04 +0200

    update dependencies

--- tests/__mocks__/starcraft2-api.ts
+++ tests/__mocks__/starcraft2-api.ts
@@ -1,157 +1,113 @@
-class StarCraft2API {
-  // eslint-disable-next-line class-methods-use-this
-  queryGrandmasterLeaderboard(): Promise<object> {
-    return Promise.resolve({
-      ladderTeams: [
-        {
-          teamMembers: [
-            {
-              displayName: 'sample grandmaster leaderboard object',
-            },
-          ],
-        },
-      ],
-    });
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  querySeason(): Promise<object> {
-    return Promise.resolve({
-      seasonId: 9999,
-    });
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  queryLegacyAchievements(): Promise<object> {
-    return Promise.resolve({
-      achievements: [
-        {
-          title: 'sample legacy achievement',
-        },
-      ],
-    });
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  queryLegacyRewards(): Promise<object> {
-    return Promise.resolve({
-      portraits: [
-        {
-          title: 'sample legacy reward',
-        },
-      ],
-    });
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  queryLegacyLadder(): Promise<object> {
-    return Promise.resolve({
-      ladderMembers: [
-        {
-          character: {
-            displayName: 'sample legacy ladder object',
+export const StarCraft2API = jest.fn().mockImplementation(() => ({
+  queryGrandmasterLeaderboard: () => Promise.resolve({
+    ladderTeams: [
+      {
+        teamMembers: [
+          {
+            displayName: 'sample grandmaster leaderboard object',
           },
-        },
-      ],
-    });
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  queryLegacyLadders(): Promise<object> {
-    return Promise.resolve({
-      currentSeason: [
-        {
-          ladder: [
-            {
-              ladderName: 'sample legacy ladders object',
-            },
-          ],
-        },
-      ],
-    });
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  queryLegacyMatchHistory(): Promise<object> {
-    return Promise.resolve({
-      matches: [
-        {
-          map: 'sample legacy match history object',
-        },
-      ],
-    });
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  queryLegacyProfile(): Promise<object> {
-    return Promise.resolve({
-      displayName: 'sample legacy profile object',
-    });
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  queryPlayerLadder(): Promise<object> {
-    return Promise.resolve({
-      ladderTeams: [
-        {
-          teamMembers: [
-            {
-              displayName: 'sample ladder object',
-            },
-          ],
-        },
-      ],
-    });
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  queryProfileMetadata(): Promise<object> {
-    return Promise.resolve({
-      name: 'sample profile metadata object',
-    });
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  queryLadderSummary(): Promise<object> {
-    return Promise.resolve({
-      showCaseEntries: [
-        {
-          ladderId: 'sample ladder summary object',
-        },
-      ],
-    });
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  queryStaticProfileData(): Promise<object> {
-    return Promise.resolve({
-      achievements: [
-        {
-          description: 'sample static profile data object',
-        },
-      ],
-    });
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  queryProfile(): Promise<object> {
-    return Promise.resolve({
-      summary: {
-        displayName: 'sample profile object',
+        ],
       },
-    });
-  }
+    ],
+  }),
 
-  // eslint-disable-next-line class-methods-use-this
-  queryLeagueData(): Promise<object> {
-    return Promise.resolve({
-      _links: {
-        self: {
-          href: 'sample league object',
+  querySeason: () => Promise.resolve({
+    seasonId: 9999,
+  }),
+
+  queryLegacyAchievements: () => Promise.resolve({
+    achievements: [
+      {
+        title: 'sample legacy achievement',
+      },
+    ],
+  }),
+
+  queryLegacyRewards: () => Promise.resolve({
+    portraits: [
+      {
+        title: 'sample legacy reward',
+      },
+    ],
+  }),
+
+  queryLegacyLadder: () => Promise.resolve({
+    ladderMembers: [
+      {
+        character: {
+          displayName: 'sample legacy ladder object',
         },
       },
-    });
-  }
-}
+    ],
+  }),
 
-export default StarCraft2API;
+  queryLegacyLadders: () => Promise.resolve({
+    currentSeason: [
+      {
+        ladder: [
+          {
+            ladderName: 'sample legacy ladders object',
+          },
+        ],
+      },
+    ],
+  }),
+
+  queryLegacyMatchHistory: () => Promise.resolve({
+    matches: [
+      {
+        map: 'sample legacy match history object',
+      },
+    ],
+  }),
+
+  queryLegacyProfile: () => Promise.resolve({
+    displayName: 'sample legacy profile object',
+  }),
+
+  queryPlayerLadder: () => Promise.resolve({
+    ladderTeams: [
+      {
+        teamMembers: [
+          {
+            displayName: 'sample ladder object',
+          },
+        ],
+      },
+    ],
+  }),
+
+  queryProfileMetadata: () => Promise.resolve({
+    name: 'sample profile metadata object',
+  }),
+
+  queryLadderSummary: () => Promise.resolve({
+    showCaseEntries: [
+      {
+        ladderId: 'sample ladder summary object',
+      },
+    ],
+  }),
+
+  queryStaticProfileData: () => Promise.resolve({
+    achievements: [
+      {
+        description: 'sample static profile data object',
+      },
+    ],
+  }),
+
+  queryProfile: () => Promise.resolve({
+    summary: {
+      displayName: 'sample profile object',
+    },
+  }),
+
+  queryLeagueData: () => Promise.resolve({
+    _links: {
+      self: {
+        href: 'sample league object',
+      },
+    },
+  }),
+}));
