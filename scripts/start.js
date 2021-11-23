6ac1f615bfa386f762ae12f9b98976347dd61e73
Łukasz Wójcik <lwojcik@lukem.net>
2021-11-23 18:46:20 +0100

    Signal handling

--- scripts/start.js
+++ scripts/start.js
@@ -202,6 +202,30 @@ if (process.env.SAS_REDIS_ENABLE === 'true') {
   });
 }
 
+const handle = (signal) => {
+  // eslint-disable-next-line no-console
+  console.log(`*^!@4=> Received event: ${signal}`);
+};
+
+// eslint-disable-next-line jest/require-hook
+process.on('SIGHUP', handle);
+
+const closeGracefully = async (signal) => {
+  // eslint-disable-next-line no-console
+  console.log(`*^!@4=> Received signal to terminate: ${signal}`);
+
+  await fastifyInstance.close();
+  // await db.close() if we have a db connection in this app
+  // await other things we should cleanup nicely
+  process.exit();
+};
+
+// eslint-disable-next-line jest/require-hook
+process.on('SIGINT', closeGracefully);
+
+// eslint-disable-next-line jest/require-hook
+process.on('SIGTERM', closeGracefully);
+
 fastifyInstance.register(server, opts);
 fastifyInstance.register(fastifyBlipp);
 
