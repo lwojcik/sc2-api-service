3470d4a508eb62fe8aa68ace8d1117cefeac595a
Łukasz Wójcik <lwojcik@lukem.net>
2022-07-03 21:29:55 +0200

    Update StarCraft2API

--- src/common/constants/index.ts
+++ src/common/constants/index.ts
@@ -3,3 +3,4 @@ export * from './APP_INFO';
 export * from './CONFIG_VALIDATION_SCHEMA';
 export * from './DEFAULTS';
 export * from './DATA_KEYS';
+export * from './SC2API_METHOD_MAPPINGS';
--- src/starcraft2api/starcraft2api.service.ts
+++ src/starcraft2api/starcraft2api.service.ts
@@ -1,14 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { StarCraft2API } from 'starcraft2-api';
+import { SC2API_METHOD_MAPPINGS } from '../common/constants';
+import { Sc2DataKey } from '../common/types';
 import { LoggerService } from '../logger/logger.service';
 
 @Injectable()
 export class StarCraft2ApiService {
+  private starcraft2api: StarCraft2API;
+
   constructor(private readonly logger: LoggerService) {
     this.logger.setLoggedClass(StarCraft2ApiService.name);
+
+    this.starcraft2api = new StarCraft2API({
+      region: 'us',
+      clientId: 'a',
+      clientSecret: 'a',
+    });
   }
 
   get(key: string, args: unknown) {
     this.logger.setLoggedMethod(this.get.name, { key, args });
+    this.logger.debug();
+
+    if (!Object.values(Sc2DataKey).includes(key as Sc2DataKey)) {
+      throw new RangeError(
+        `'${key}' does not exist as a value in Sc2DataKey enum.`
+      );
+    }
+
+    const sc2ApiMethod = SC2API_METHOD_MAPPINGS[key];
+    console.log(sc2ApiMethod);
+    console.log(args);
 
     return { key, args };
   }
