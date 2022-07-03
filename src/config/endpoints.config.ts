d55222e989404bd0eb6d4c072144c557288423ac
Łukasz Wójcik <lwojcik@lukem.net>
2022-07-03 12:41:33 +0200

    Endpoint list on /

--- src/config/endpoints.config.ts
+++ src/config/endpoints.config.ts
@@ -5,12 +5,56 @@ export const endpointsConfig = registerAs('endpoints', () => ({
     url: '/status',
     method: 'GET',
   },
-  accesstoken: {
-    url: '/accesstoken',
+  getLeague: {
+    url: '/data/league/:seasonId/:queueId/:teamType/:leagueId',
     method: 'GET',
   },
-  accesstokenrefresh: {
-    url: '/accesstoken?refresh=true',
+  getGrandmaster: {
+    url: '/ladder/grandmaster/:regionId',
+    method: 'GET',
+  },
+  getSeason: {
+    url: '/ladder/season/:regionId',
+    method: 'GET',
+  },
+  getStatic: {
+    url: '/profile/static/:regionId',
+    method: 'GET',
+  },
+  getMetadata: {
+    url: '/profile/metadata/:regionId/:realmId/:profileId',
+    method: 'GET',
+  },
+  getProfile: {
+    url: '/profile/profile/:regionId/:realmId/:profileId',
+    method: 'GET',
+  },
+  getLadderSummary: {
+    url: '/profile/laddersummary/:regionId/:realmId/:profileId',
+    method: 'GET',
+  },
+  getPlayerLadder: {
+    url: '/profile/ladder/:regionId/:realmId/:profileId/:ladderId',
+    method: 'GET',
+  },
+  getLegacyProfile: {
+    url: '/legacy/profile/:regionId/:realmId/:profileId',
+    method: 'GET',
+  },
+  getLegacyLadders: {
+    url: '/legacy/ladders/:regionId/:realmId/:profileId',
+    method: 'GET',
+  },
+  getLegacyMatches: {
+    url: '/legacy/matches/:regionId/:realmId/:profileId',
+    method: 'GET',
+  },
+  getLegacyLadder: {
+    url: '/legacy/ladder/:ladderId',
+    method: 'GET',
+  },
+  getLegacyAchievements: {
+    url: '/achievements/:regionId',
     method: 'GET',
   },
 }));
