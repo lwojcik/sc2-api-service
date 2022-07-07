48da8775aab9941c76849c4c8dd5ec2cfc879402
Łukasz Wójcik <lwojcik@lukem.net>
2022-07-08 00:05:10 +0200

    More unit test coverage

--- src/databroker/databroker.service.spec.ts
+++ src/databroker/databroker.service.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ModuleMocker } from 'jest-mock';
+import { StarCraft2ApiService } from '../starcraft2api/starcraft2api.service';
+import { Sc2DataKey } from '../common/types';
 import { DataBrokerService } from './databroker.service';
+import { CacheService } from '../cache/cache.service';
 
 const moduleMocker = new ModuleMocker(global);
 
@@ -12,6 +15,30 @@ describe('DataBrokerService', () => {
       providers: [DataBrokerService],
     })
       .useMocker((token) => {
+        if (token === CacheService) {
+          return {
+            get: () =>
+              Promise.resolve({
+                statusCode: 200,
+                created: 1,
+                data: {
+                  foo: 'sample_data_from_cache',
+                },
+              }),
+          };
+        }
+        if (token === StarCraft2ApiService) {
+          return {
+            get: () =>
+              Promise.resolve({
+                statusCode: 200,
+                created: 2000,
+                data: {
+                  foo: 'sample_data_from_sc2api_service_mock',
+                },
+              }),
+          };
+        }
         if (typeof token === 'function') {
           const mockMetadata = moduleMocker.getMetadata(token);
           const Mock = moduleMocker.generateFromMetadata(mockMetadata);
@@ -26,5 +53,26 @@ describe('DataBrokerService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should get data by key', async () => {
+    expect(
+      await service.getData({
+        key: Sc2DataKey.getGrandmaster,
+        args: { region: 'us' },
+      })
+    ).toMatchSnapshot();
+  });
+
+  it('should get data if refresh is forced', async () => {
+    expect(
+      await service.getData(
+        {
+          key: Sc2DataKey.getGrandmaster,
+          args: { region: 'us' },
+        },
+        true
+      )
+    ).toMatchSnapshot();
   });
 });
